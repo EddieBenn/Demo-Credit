70bdd639c8a3d90c4ead39294cd27aeb68c48a6f
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-10-18 12:45:15 +0100

    auth module and location counter module completed. Migrations also successful

--- src/base.entity.ts
+++ src/base.entity.ts
@@ -1,5 +1,26 @@
+import { Model } from 'objection';
+
+export class BaseEntity extends Model {
+  static get idColumn() {
+    return 'id';
+  }
+
+  static get timestamps() {
+    return true;
+  }
+
+  id?: string;
+  created_at?: Date | string;
+  updated_at?: Date | string;
+  deleted_at?: Date | string | null;
+}
+
 export enum RolesEnum {
   USER = 'user',
+  ADMIN = 'admin',
+}
+
+export enum ADMIN_ROLES {
   ADMIN = 'admin',
 }
 
