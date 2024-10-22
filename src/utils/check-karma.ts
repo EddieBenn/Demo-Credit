a0f8713bea57102e9cad3f0d597e82cb5a8f099e
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-10-22 08:01:04 +0100

    karma check added to user creation

---
+++ src/utils/check-karma.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { ADJUTOR_SECRET_KEY, ADJUTOR_KARMA_URL } = process.env;
+
+export const checkKarmaList = async (email: string) => {
+  const url = `${ADJUTOR_KARMA_URL}/${email}`;
+  const headers = {
+    Authorization: `Bearer ${ADJUTOR_SECRET_KEY}`,
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    const response = await axios.get(url, { headers });
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || 'Error checking Karma list',
+    );
+  }
+};
