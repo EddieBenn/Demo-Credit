e3ebe1d9026b69e9d82497a5d58acfe72f187777
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-10-23 20:45:51 +0100

    all unit tests completed

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Post, Body, Param, Res, Get } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Post, Body, Param, Get } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import {
   ConfirmTransferDto,
@@ -30,35 +29,39 @@ export class PaymentsController {
   @ApiUnprocessableEntityResponse()
   @ApiSecurity('access_token')
   @Post('initialize/paystack')
-  async initializeTransactionPaystack(
-    @Body() body: CreatePaymentDto,
-    @Res() res: Response,
-  ) {
+  async initializeTransactionPaystack(@Body() body: CreatePaymentDto) {
     const { email, amount } = body;
-    const transaction =
-      await this.paymentsService.initializeTransactionPaystack(email, amount);
-    return res.status(200).json(transaction);
+    try {
+      return await this.paymentsService.initializeTransactionPaystack(
+        email,
+        amount,
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 
   @ApiOperation({ summary: 'Verify Payment' })
   @ApiUnprocessableEntityResponse()
   @ApiSecurity('access_token')
   @Post('verify/:reference')
-  async verifyTransactionPaystack(
-    @Param('reference') reference: string,
-    @Res() res: Response,
-  ) {
-    const transaction =
-      await this.paymentsService.verifyTransactionPaystack(reference);
-    return res.status(200).json(transaction);
+  async verifyTransactionPaystack(@Param('reference') reference: string) {
+    try {
+      return await this.paymentsService.verifyTransactionPaystack(reference);
+    } catch (error) {
+      throw error;
+    }
   }
 
   @ApiOperation({ summary: 'Fulfil Payment Webhook' })
   @ApiSecurity('access_token')
   @Post('webhook/paystack')
-  async handlePaystackWebhook(@Body() body: any, @Res() res: Response) {
-    const webhook = await this.paymentsService.handlePaystackWebhook(body);
-    return res.status(200).json(webhook);
+  async handlePaystackWebhook(@Body() body: any) {
+    try {
+      return await this.paymentsService.handlePaystackWebhook(body);
+    } catch (error) {
+      throw error;
+    }
   }
 
   @ApiOperation({ summary: 'Create Transfer Recipient on Paystack' })
@@ -67,17 +70,14 @@ export class PaymentsController {
   @Post('transfer-recipient/paystack')
   async createTransferRecipientPaystack(
     @Body() body: CreateTransferRecipientDto,
-    @Res() res: Response,
   ) {
     const { accountNumber, bankCode, name } = body;
     try {
-      const recipient =
-        await this.paymentsService.createTransferRecipientPaystack(
-          accountNumber,
-          bankCode,
-          name,
-        );
-      return res.status(200).json(recipient);
+      return await this.paymentsService.createTransferRecipientPaystack(
+        accountNumber,
+        bankCode,
+        name,
+      );
     } catch (error) {
       throw error;
     }
@@ -87,19 +87,15 @@ export class PaymentsController {
   @ApiUnprocessableEntityResponse()
   @ApiSecurity('access_token')
   @Post('initiate-transfer/paystack')
-  async initiateTransferPaystack(
-    @Body() body: InitiateTransferDto,
-    @Res() res: Response,
-  ) {
+  async initiateTransferPaystack(@Body() body: InitiateTransferDto) {
     const { amount, recipientCode, email, reason } = body;
     try {
-      const transfer = await this.paymentsService.initiateTransferPaystack(
+      return await this.paymentsService.initiateTransferPaystack(
         amount,
         recipientCode,
         email,
         reason,
       );
-      return res.status(200).json(transfer);
     } catch (error) {
       throw error;
     }
@@ -109,17 +105,13 @@ export class PaymentsController {
   @ApiUnprocessableEntityResponse()
   @ApiSecurity('access_token')
   @Post('confirm-transfer')
-  async confirmTransferPaystack(
-    @Body() body: ConfirmTransferDto,
-    @Res() res: Response,
-  ) {
+  async confirmTransferPaystack(@Body() body: ConfirmTransferDto) {
     const { transferCode, otp } = body;
     try {
-      const result = await this.paymentsService.confirmTransferPaystack(
+      return await this.paymentsService.confirmTransferPaystack(
         transferCode,
         otp,
       );
-      return res.status(200).json(result);
     } catch (error) {
       throw error;
     }
@@ -129,14 +121,9 @@ export class PaymentsController {
   @ApiUnprocessableEntityResponse()
   @ApiSecurity('access_token')
   @Post('verify-transfer/:reference')
-  async verifyTransferPaystack(
-    @Param('reference') reference: string,
-    @Res() res: Response,
-  ) {
+  async verifyTransferPaystack(@Param('reference') reference: string) {
     try {
-      const result =
-        await this.paymentsService.verifyTransferPaystack(reference);
-      return res.status(200).json(result);
+      return await this.paymentsService.verifyTransferPaystack(reference);
     } catch (error) {
       throw error;
     }
@@ -151,15 +138,14 @@ export class PaymentsController {
   @ApiUnprocessableEntityResponse()
   @ApiSecurity('access_token')
   @Post('transfer')
-  async transferFunds(@Body() body: TransferFundsDto, @Res() res: Response) {
+  async transferFunds(@Body() body: TransferFundsDto) {
     const { senderAccountNumber, receiverAccountNumber, amount } = body;
     try {
-      const result = await this.paymentsService.transferFunds(
+      return await this.paymentsService.transferFunds(
         senderAccountNumber,
         receiverAccountNumber,
         amount,
       );
-      return res.status(200).json(result);
     } catch (error) {
       throw error;
     }
@@ -169,10 +155,9 @@ export class PaymentsController {
   @ApiUnprocessableEntityResponse()
   @ApiSecurity('access_token')
   @Get('customers/paystack')
-  async listCustomersPaystack(@Res() res: Response) {
+  async listCustomersPaystack() {
     try {
-      const result = await this.paymentsService.listCustomersPaystack();
-      return res.status(200).json(result);
+      return await this.paymentsService.listCustomersPaystack();
     } catch (error) {
       throw error;
     }
@@ -182,13 +167,9 @@ export class PaymentsController {
   @ApiUnprocessableEntityResponse()
   @ApiSecurity('access_token')
   @Post('customer/get-one/:email')
-  async fetchOneCustomerPaystack(
-    @Param('email') email: string,
-    @Res() res: Response,
-  ) {
+  async fetchOneCustomerPaystack(@Param('email') email: string) {
     try {
-      const result = await this.paymentsService.fetchOneCustomerPaystack(email);
-      return res.status(200).json(result);
+      return await this.paymentsService.fetchOneCustomerPaystack(email);
     } catch (error) {
       throw error;
     }
@@ -198,19 +179,15 @@ export class PaymentsController {
   @ApiUnprocessableEntityResponse()
   @ApiSecurity('access_token')
   @Post('create-customer/paystack')
-  async createCustomerPaystack(
-    @Body() body: CreateCustomerDto,
-    @Res() res: Response,
-  ) {
+  async createCustomerPaystack(@Body() body: CreateCustomerDto) {
     const { email, firstName, lastName, phoneNumber } = body;
     try {
-      const recipient = await this.paymentsService.createCustomerPaystack(
+      return await this.paymentsService.createCustomerPaystack(
         email,
         firstName,
         lastName,
         phoneNumber,
       );
-      return res.status(200).json(recipient);
     } catch (error) {
       throw error;
     }
@@ -222,12 +199,11 @@ export class PaymentsController {
   @Post('create/virtual-account/:customerCode')
   async createDedicatedVirtualAccount(
     @Param('customerCode') customerCode: string,
-    @Res() res: Response,
   ) {
     try {
-      const result =
-        await this.paymentsService.createDedicatedVirtualAccount(customerCode);
-      return res.status(200).json(result);
+      return await this.paymentsService.createDedicatedVirtualAccount(
+        customerCode,
+      );
     } catch (error) {
       throw error;
     }
